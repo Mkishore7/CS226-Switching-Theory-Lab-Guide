043477055e2bf9bfcb4689050f232a56e7edf806
Siddharth Thakur <codeblooded29@gmail.com>
2018-05-12 09:33:04 +0530

    Add files via upload

---
+++ Lab10/test_clock.v
@@ -0,0 +1,13 @@
+module test_clock();
+parameter half_cycle = 50;
+reg clock;
+initial
+begin: clock_label // Note: clock_loop is a named block of statements
+forever
+begin
+#half_cycle clock = 1;
+#half_cycle clock = 0;
+end
+end
+// always #100 clock = ~clock; another way of declaring clock
+endmodule
